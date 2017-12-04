8a6b3036dd70ca47c2cb7775834f2d06f825b255
Ken <32885973+ykzheng2@users.noreply.github.com>
2017-12-04 16:23:35 -0500

    Add files via upload

---
+++ app/src/main/java/com/example/bustudyspots/MapActivity.java
@@ -0,0 +1,29 @@
+package com.example.bustudyspots;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+
+public class MapActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.map_activity);
+
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == android.R.id.home) {
+            this.finish();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+}
